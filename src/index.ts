55b59d036d242c3c6adf3f4745cb3df521a9859b
Hoaper <beeboplay@gmail.com>
2023-12-14 16:42:50 +0600

    books json and images loaded

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import express from 'express'
 
 const app = express();
 const port = 5000;
+
+interface Book {
+    id: number;
+    title: string;
+    year: number;
+
+}
+
+const book = {
+
+};
+
 app.get('/', (request, response) => {
     response.send('Hello world!');
 });
