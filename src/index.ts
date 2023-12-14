ef2be7b6bffd0fd3f959e095e7802387b372a740
Hoaper <beeboplay@gmail.com>
2023-12-14 17:00:44 +0600

    book type added

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,13 @@
 import express from 'express'
+import {Book} from "../types/book";
 
 const app = express();
 const port = 5000;
 
-interface Book {
-    id: number;
-    title: string;
-    year: number;
-
-}
-
-const book = {
-
-};
-
 app.get('/', (request, response) => {
     response.send('Hello world!');
 });
+
 app.listen(port, () => {
     console.log(`Running super-duper backend on port ${port}`)
 });
